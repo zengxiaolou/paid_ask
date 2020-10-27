95deea685d3ccc9f4027410aac6a001a63124d2a
zengxiaolou <44358506+zengxiaolou@users.noreply.github.com>
2020-10-27 10:01:47 +0800

    test

--- main.go
+++ main.go
@@ -12,13 +12,32 @@ import (
 	"github.com/kataras/iris/v12/context"
 )
 
-func main() {
+//
+//func main() {
+//	app := iris.New()
+//	app.Handle("GET","/", func(context context.Context) {
+//		_, _ = context.WriteString("<h1>Welcome</h1>")
+//	})
+//	app.Handle("GET","/ping", func(context context.Context) {
+//		_, _ = context.JSON(iris.Map{"message": "hello"})
+//	})
+//	_ = app.Run(iris.Addr(":8002"))
+//}
+
+func main()  {
 	app := iris.New()
-	app.Handle("GET","/", func(context context.Context) {
-		_, _ = context.WriteString("<h1>Welcome</h1>")
+	app.Get("/", func(context context.Context) {
+		_,_ = context.HTML("<b>hello</b>")
 	})
-	app.Handle("GET","/ping", func(context context.Context) {
-		_, _ = context.JSON(iris.Map{"message": "hello"})
-	})
-	_ = app.Run(iris.Addr(":8002"))
+	app.Configure(iris.WithConfiguration(iris.Configuration{DisableStartupLog: false}))
+	_ = app.Run(iris.Addr(":8002"), iris.WithConfiguration(iris.Configuration{
+		DisableInterruptHandler: false,
+		DisablePathCorrection: false,
+		EnablePathEscape: false,
+		FireMethodNotAllowed: false,
+		DisableBodyConsumptionOnUnmarshal: false,
+		DisableAutoFireStatusCode: false,
+		TimeFormat: "Mon, 02 Jan 2006 15:04:05 GMT",
+		Charset: "UTF-8",
+	}))
 }
